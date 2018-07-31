d74b27cab2b36ecca304deea9a6bab9acaf0a934
Mark Chambers <mwchambers@gmail.com>
2018-07-31 16:01:31 +0100

    Try to detect chrome OS and report device is secure as KeyguardManager.isDeviceSecure() is not returning true even when there is a password / pin set.

--- src/android/PinCheck.java
+++ src/android/PinCheck.java
@@ -7,15 +7,21 @@ import android.os.Build;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
+import android.os.Build;
+
 
 /**
  * This class returns a string depending on whether the keyguard is detected on Android. This method is called from JavaScript.
  */
 public class PinCheck extends CordovaPlugin {
     private static final boolean isDeviceSecureSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    private static String TAG = "PinCheck";
+    private static final String ARC_DEVICE_PATTERN = ".+_cheets|cheets_.+";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -30,17 +36,35 @@ public class PinCheck extends CordovaPlugin {
         Context context = this.cordova.getActivity().getApplicationContext();
         boolean result = isDeviceSecure(context);
         if (result) {
+            Log.d(TAG, "device IS secure");
             callbackContext.success("PIN_SETUP");
         } else {
+            Log.d(TAG, "device NOT secure");
             callbackContext.error("NO_PIN_SETUP");
         }
     }
 
     private boolean isDeviceSecure(Context context) {
+        // https://stackoverflow.com/questions/39784415/how-to-detect-programmatically-if-android-app-is-running-in-chrome-book-or-in
+        if( context.getPackageManager().hasSystemFeature("org.chromium.arc.device_management")) { 
+            Log.d(TAG, "has arc.device_management ");
+            return true;
+        }
+
+        // See: https://github.com/google/talkback/blob/e69d4731fce02bb9e69613d0e48c29033cad4a98/utils/src/main/java/FormFactorUtils.java#L51
+        if(Build.DEVICE != null) { 
+            Log.d(TAG, "Build.DEVICE: " + Build.DEVICE);
+            if(Build.DEVICE.matches(ARC_DEVICE_PATTERN)) { 
+                return true;
+            }
+        }
+
         KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
         if (PinCheck.isDeviceSecureSupported) {
+            Log.d(TAG, "calling KeyguardManager.isDeviceSecure()");
             return keyguardManager.isDeviceSecure();
         } else {
+            Log.d(TAG, "calling KeyguardManager.isKeyguardSecure()");
             return keyguardManager.isKeyguardSecure();
         }
     }
